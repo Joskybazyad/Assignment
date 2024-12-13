b06ab5110ca508be55bfa32fdfbe933767d30a1f
user <user@DESKTOP-A0S5LT4>
2024-12-13 22:45:29 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1:Write a program that allows the user to enter a number and then print it
+            //Console.WriteLine("Please Enter A Number:");
+            //string input = Console.ReadLine();
+            //int number = 0;
+            //number=int.Parse(input);
+            //Console.WriteLine($"the number you entered is :{number}");
+            #endregion
+
+            #region Q2:Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+            //int x = 10;
+            //int y = 0;
+            //Console.WriteLine("before assigning");
+            //Console.WriteLine($"X = {x}");
+            //Console.WriteLine($"Y = {y}");
+            //y = x;
+            //// y will take a copy of x value 
+            //Console.WriteLine("after assigning");
+            //Console.WriteLine($"X = {x}");
+            //Console.WriteLine($"Y = {y}");
+            //x = 12;
+            //// the value of x will change to 12
+            //Console.WriteLine("after modifing");
+            //Console.WriteLine($"X = {x}");
+            //Console.WriteLine($"Y = {y}");
+
+            #endregion
+
+            #region Q3:Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+            //Point P1 = new Point();
+            //Point P2 = new Point();
+            //P1.x = 5;
+            //P1.y = 4;
+            //P2 = P1;
+            ////P2 will take the same address that points to P1 values
+            //Console.WriteLine("after assigning");
+            //Console.WriteLine($"P1 x:{P1.x}");
+            //Console.WriteLine($"P1 y:{P1.y}");
+            //Console.WriteLine($"P2 x:{P2.x}");
+            //Console.WriteLine($"P2 y:{P2.y}");
+            //P2.x=2;
+            //P2.y=3;
+            //// the values will change in P1 and P2
+            //Console.WriteLine("after modifing");
+            //Console.WriteLine($"P1 x:{P1.x}");
+            //Console.WriteLine($"P1 y:{P1.y}");
+            //Console.WriteLine($"P2 x:{P2.x}");
+            //Console.WriteLine($"P2 y:{P2.y}");
+
+
+
+            #endregion
+        }
+    }
+}
